a843625e0b2d80be392f929ca5ac8f70990f7b7a
Abel Haro <ahararm@upv.es>
2024-11-24 17:00:31 +0100

    Reorganización de scripts

--- scripts/inference_tracker_custom_parallel.py
+++ scripts/inference_tracker_custom_parallel.py
@@ -3,19 +3,21 @@ from ultralytics import YOLO
 import os
 import threading
 from queue import Queue
+from ultralytics.trackers.byte_tracker import BYTETracker
+from argparse import Namespace
 
 # Variables globales para el tiempo
 total_time_capturing = 0
 total_time_processing = 0
+total_time_tracking = 0
 total_time_writing = 0
+times_detect_function = {"preprocess": 0, "inference": 0, "postprocess": 0}
 
-gpu_time = {"preprocess": 0, "inference": 0, "postprocess": 0}
 
 def get_total_frames(video_path):
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
-        print("Error al abrir el archivo de video.")
-        return -1
+        raise IOError(f"Error al abrir el archivo de video: {video_path}")
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     cap.release()
     return total_frames
@@ -25,7 +27,7 @@ def update_memory(track_id, detected_class, memory):
         memory[track_id] = {'defective': detected_class.endswith('-d'), 'visible_frames': 30}
     else:
         memory[track_id]['defective'] |= detected_class.endswith('-d')
-        memory[track_id]['visible_frames'] = 30  # Reset the frame counter
+        memory[track_id]['visible_frames'] = 30  # Reset counter
 
         if memory[track_id]['defective'] and not detected_class.endswith('-d'):
             detected_class = detected_class + '-d'
@@ -35,6 +37,8 @@ def update_memory(track_id, detected_class, memory):
 def capture_frames(video_path, frame_queue):
     global total_time_capturing
     cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        raise IOError(f"Error al abrir el archivo de video: {video_path}")
     while cap.isOpened():
         t1 = cv2.getTickCount()
         ret, frame = cap.read()
@@ -46,57 +50,90 @@ def capture_frames(video_path, frame_queue):
     cap.release()
     frame_queue.put(None)  # Señal de finalización
 
-def process_frames(frame_queue, result_queue, model):
-    global total_time_processing
+def process_frames(frame_queue, detection_queue, model):
+    global total_time_processing, times_detect_function
     while True:
         frame = frame_queue.get()
         if frame is None:  # Señal de finalización
-            result_queue.put(None)
+            detection_queue.put(None)
             break
         t1 = cv2.getTickCount()
-        results = model.track(source=frame, device=0, persist=True, task='detect', tracker='bytetrack.yaml')
+        results = model.predict(source=frame, device=0, task='detect')
         t2 = cv2.getTickCount()
         total_time_processing += (t2 - t1) / cv2.getTickFrequency()
+        detection_queue.put((frame, results[0]))
+        times_detect_function["preprocess"] += results[0].speed["preprocess"]
+        times_detect_function["inference"] += results[0].speed["inference"]
+        times_detect_function["postprocess"] += results[0].speed["postprocess"]
 
-        result_queue.put((frame, results))
+def tracking_frames(detection_queue, tracking_queue):
+    global total_time_tracking
+    args = Namespace(
+        tracker_type='bytetrack',
+        track_high_thresh=0.25,
+        track_low_thresh=0.1,
+        new_track_thresh=0.25,
+        track_buffer=30,
+        match_thresh=0.8,
+        fuse_score=True
+    )
+    tracker = BYTETracker(args, frame_rate=30)
 
-def draw_and_write_frames(result_queue, output_video_path, classes, memory, colors):
+    class Detections:
+        def __init__(self, boxes, confidences, class_ids):
+            self.conf = confidences
+            self.xywh = boxes
+            self.cls = class_ids
+            
+    while True:
+        item = detection_queue.get()
+        if item is None:
+            tracking_queue.put(None) # Señal de finalización
+            break
+
+        t1 = cv2.getTickCount()
+        frame, result = item
+
+        detections = Detections(
+            result.boxes.xywh.cpu().numpy(),
+            result.boxes.conf.cpu().numpy(),
+            result.boxes.cls.cpu().numpy().astype(int)
+        )
+        
+        outputs = tracker.update(detections, frame)
+        t2 = cv2.getTickCount()
+        total_time_tracking += (t2 - t1) / cv2.getTickFrequency()
+        tracking_queue.put((frame, outputs))
+
+def draw_and_write_frames(tracking_queue, output_video_path, classes, memory, colors):
     global total_time_writing
     out = None
     while True:
-        item = result_queue.get()
-        if item is None:  # Señal de finalización
+        item = tracking_queue.get()
+        if item is None:
             break
         t1 = cv2.getTickCount()
-        frame, results = item
-        
-        gpu_time['preprocess'] += results[0].speed['preprocess']
-        gpu_time['inference'] += results[0].speed['inference']
-        gpu_time['postprocess'] += results[0].speed['postprocess']
-        
+        frame, tracked_objects = item
+
         if out is None:
             frame_height, frame_width = frame.shape[:2]
             fourcc = cv2.VideoWriter_fourcc(*'mp4v')
             out = cv2.VideoWriter(output_video_path, fourcc, 20, (frame_width, frame_height))
 
-        if results[0].boxes.id is not None:
-            boxes = results[0].boxes.xyxy.cpu().numpy().astype(int)
-            ids = results[0].boxes.id.cpu().numpy().astype(int)
-            detected_classes = results[0].boxes.cls.cpu().numpy().astype(int)
-            confidences = results[0].boxes.conf.cpu().numpy().astype(float)
+        for obj in tracked_objects:
+            xmin, ymin, xmax, ymax, obj_id = map(int, obj[:5])  # Asignamos las primeras 5 posiciones a enteros
+            conf = float(obj[5])  # Convertimos el valor de conf a float
+            cls = int(obj[6])  # Asignamos cls como entero
+         
+            detected_class = classes[cls]
 
-            for box, obj_id, cls, conf in zip(boxes, ids, detected_classes, confidences):
-                xmin, ymin, xmax, ymax = box
-                detected_class = classes[cls]
+            update_memory(obj_id, detected_class, memory)
+            detected_class = memory[obj_id]['class']
+            color = colors.get(detected_class, (255, 255, 255))
 
-                update_memory(obj_id, detected_class, memory)
-
-                detected_class = memory[obj_id]['class']
-                color = colors.get(detected_class, (255, 255, 255))
-
-                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color, 2)
-                text = f'ID:{obj_id} {detected_class} {conf:.2f}'
-                cv2.putText(frame, text, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color, 2)
+            text = f'ID:{obj_id} {detected_class} {conf:.2f}'
+            cv2.putText(frame, text, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
         for track_id in list(memory):
             memory[track_id]['visible_frames'] -= 1
@@ -111,50 +148,52 @@ def draw_and_write_frames(result_queue, output_video_path, classes, memory, colo
         out.release()
 
 def main():
-    global total_time_capturing, total_time_processing, total_time_writing
     model_path = '../models/canicas/2024_11_15/2024_11_15_canicas_yolo11n.engine'
     video_path = '../datasets_labeled/videos/video_general_defectos_3.mp4'
     output_dir = '../inference_predictions/custom_tracker'
     os.makedirs(output_dir, exist_ok=True)
     output_video_path = os.path.join(output_dir, 'video_con_tracking.mp4')
 
-    model = YOLO(model_path)
     classes = {0: 'negra', 1: 'blanca', 2: 'verde', 3: 'azul', 4: 'negra-d', 5: 'blanca-d', 6: 'verde-d', 7: 'azul-d'}
     colors = {
         'negra': (0, 0, 255), 'blanca': (0, 255, 0), 'verde': (255, 0, 0), 'azul': (255, 255, 0),
         'negra-d': (0, 165, 255), 'blanca-d': (255, 165, 0), 'verde-d': (255, 105, 180), 'azul-d': (255, 0, 255)
     }
     memory = {}
+    
+    model = YOLO(model_path)
 
     frame_queue = Queue(maxsize=10)
-    result_queue = Queue(maxsize=10)
+    detection_queue = Queue(maxsize=10)
+    tracking_queue = Queue(maxsize=10)
 
     threads = [
         threading.Thread(target=capture_frames, args=(video_path, frame_queue)),
-        threading.Thread(target=process_frames, args=(frame_queue, result_queue, model)),
-        threading.Thread(target=draw_and_write_frames, args=(result_queue, output_video_path, classes, memory, colors))
+        threading.Thread(target=process_frames, args=(frame_queue, detection_queue, model)),
+        threading.Thread(target=tracking_frames, args=(detection_queue, tracking_queue)),
+        threading.Thread(target=draw_and_write_frames, args=(tracking_queue, output_video_path, classes, memory, colors))
     ]
 
     t1 = cv2.getTickCount()
     for thread in threads:
         thread.start()
-
     for thread in threads:
         thread.join()
-
     t2 = cv2.getTickCount()
+
     total_time = (t2 - t1) / cv2.getTickFrequency()
-    
     total_frames = get_total_frames(video_path)
     
+    total_time_detect_function = (times_detect_function["preprocess"] + times_detect_function["inference"] + times_detect_function["postprocess"]) / 1000
+
     print(f"Total de frames procesados: {total_frames}")
-    print(f"Tiempo total de procesamiento: {total_time:.3f} segundos")
-    print(f"Tiempo medio por frame: {total_time / total_frames * 1000:.3f} ms, FPS: {total_frames / total_time:.3f}")
-    print(f"Tiempo total capturando frames: {total_time_capturing:.3f} segundos")
-    print(f"Tiempo total procesando frames: {total_time_processing:.3f} segundos")
-    print(f"Tiempo total escribiendo frames: {total_time_writing:.3f} segundos")
-    print(f"Tiempos que mide la función track, preprocesamiento: {gpu_time['preprocess']/1000:.3f} s, inferencia: {gpu_time['inference']/1000:.3f} s, postprocesamiento: {gpu_time['postprocess']/1000:.3f} s")
-    print(f"Tiempo total que mide la función track: {(gpu_time['preprocess'] + gpu_time['inference'] + gpu_time['postprocess'])/1000:.3f} s")
-    
-if __name__ == "__main__":
+    print(f"Tiempo total: {total_time:.3f}s, FPS: {total_frames / total_time:.3f}")
+    print(f"Captura: {total_time_capturing:.3f}s, Procesamiento: {total_time_processing:.3f}s")
+    print(f"Tracking: {total_time_tracking:.3f}s, Escritura: {total_time_writing:.3f}s")
+    print(f"Tiempo total función detect: {total_time_detect_function:.3f}s")
+    print(f"Tiempo de preprocesamiento: {times_detect_function['preprocess']/1000:.3f}s")
+    print(f"Tiempo de inferencia: {times_detect_function['inference']/1000:.3f}s")
+    print(f"Tiempo de postprocesamiento: {times_detect_function['postprocess']/1000:.3f}s")
+
+if __name__ == '__main__':
     main()
