46fce34a9c11adc75c88db1f3ad95d583267278c
AbelHaro <ahararm@upv.es>
2025-02-05 11:09:25 +0100

    Commit de prueba desde Xavier 4

--- scripts/inference/inference.py
+++ scripts/inference/inference.py
@@ -1,14 +1,17 @@
 from ultralytics import YOLO
 import cv2
 
-model_path = '/TFG/models/canicas/2024_11_28/2024_11_28_canicas_yolo11n.pt'
+model_path = '/TFG/models/canicas/2024_11_28/2024_11_28_canicas_yolo11n_FP16_GPU.engine'
 video_path = f'../../datasets_labeled/videos/contar_objetos_40_2min.mp4'
 
-model = YOLO("yolo11n")
+model = YOLO(model_path)
 
 
-t1 = cv2.getTickCount()
-results = model.predict(source=video_path, conf=0.5, device="cuda:0")
-t2 = cv2.getTickCount()
-time = (t2 - t1) / cv2.getTickFrequency()
-print(f"Tiempo total de procesamiento: {time:.3f} segundos")
+cap = cv2.VideoCapture(video_path)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    results = model.predict(source=video_path, conf=0.5, device="cuda:0")
+    
