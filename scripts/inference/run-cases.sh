cf22f3fcb3f32cd23b134011db840f82432edce7
Abel Haro <ahararm@upv.es>
2025-05-12 09:44:35 +0200

    Actualiza archivos de Excel y modifica scripts de inferencia para mejorar la gestión de datos y la ejecución de casos. Se eliminan archivos obsoletos y se añaden nuevos archivos de resultados. Ajustes en los argumentos de entrada y en la salida de los scripts.

--- scripts/inference/run-cases.sh
+++ scripts/inference/run-cases.sh
@@ -2,12 +2,12 @@
 
 # This script runs the inference cases for the given model and dataset.
 
-model_size=("n" "s" "m")
+model_size=("n")
 precision=("FP16")
-hardware=("GPU" "DLA0")
+hardware=("GPU")
 mode=("MAXN")
 parallel=("threads" "mp" "mp_shared_memory" "mp_hardware")
-max_fps=30
+max_fps=infinite
 
 # Loop through all combinations
 for size in "${model_size[@]}"; do
@@ -16,18 +16,18 @@ for size in "${model_size[@]}"; do
             for m in "${mode[@]}"; do
                 for par in "${parallel[@]}"; do
                     echo "-------------------------------------------------------------------------------------------------------"
-                    echo "Running case: Model=${size}, Precision=${prec}, Hardware=${hw}, Mode=${m}, Parallel=${par}"
+                    echo "Running case: Model=${size}, Precision=${prec}, Hardware=${hw}, Mode=${m}, Parallel=${par}, Max FPS=${max_fps}"
                     python3 inference.py \
                         --model_size $size \
                         --precision $prec \
                         --hardware $hw \
                         --mode $m \
                         --parallel $par \
-                        --max_fps $max_fps
+                        #--max_fps $max_fps
 
                     # Check if the command was successful
                     if [ $? -ne 0 ]; then
-                        echo "Error running case: Model=${size}, Precision=${prec}, Hardware=${hw}, Mode=${m}, Parallel=${par}"
+                        echo "Error running case: Model=${size}, Precision=${prec}, Hardware=${hw}, Mode=${m}, Parallel=${par}, Max FPS=${max_fps}"
                         exit 1
                     fi
                     echo
